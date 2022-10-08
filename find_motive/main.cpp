0a3beb921624a744d33b4143eba6358e543ed6b5
clara.chalumeau <clara.chalumeau@epita.fr>
2022-10-08 20:05:55 +0200

    modif

--- find_motive/main.cpp
+++ find_motive/main.cpp
@@ -4,7 +4,7 @@ int main(int argc, char *argv[])
 {
     if (argc != 3)
     {
-        cout << "Usage: ./main [PATH] [MOTIVE]";
+        cout << "Usage: ./main [PATH] [MOTIVE]\n";
         return 1;
     }
     int num;
@@ -17,12 +17,12 @@ int main(int argc, char *argv[])
         cout<<"The file " << path << " could not be opened.\n";
         return 1;
     }
-    
+
     string mystring = string((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
-    
+
     num = find_motive(mystring, motive);
 
-    cout<<"The file " << path << " contains "<< num << " words containing the motive " << motive << '\n';
+    cout<<"The file " << path << " contains "<< num << " words containing the motive " << motive << "\n";
 
     return 0;
 }
