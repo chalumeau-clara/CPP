ee3d49edead161242f6dab07b9ac2754fb3a6b73
clara.chalumeau <clara.chalumeau@epita.fr>
2022-10-19 16:28:23 +0200

    fixing find

--- find_motive/main.cpp
+++ find_motive/main.cpp
@@ -1,28 +1,39 @@
-#include "find_motive.h"
+#include <fstream>
+#include <iostream>
+#include <string>
 
-int main(int argc, char *argv[])
+using namespace std;
+
+int main(int argc, char **argv)
 {
     if (argc != 3)
     {
-        cout << "Usage: ./main [PATH] [MOTIVE]\n";
         return 1;
     }
-    int num;
+
     string path = argv[1];
     string motive = argv[2];
 
-    ifstream file (path);
-    if (!file.is_open())
+    ifstream myfile;
+    myfile.open(path);
+
+    if (myfile.fail())
     {
-        cout<<"The file " << path << " could not be opened.\n";
+        cout << "The file " + path + " could not be opened." << '\n';
         return 1;
     }
-    
-    string mystring = string((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
-    
-    num = find_motive(mystring, motive);
 
-    cout<<"The file " << path << " contains "<< num << " words containing the motive " << motive << "\n";
+    string word;
+    size_t count = 0;
+    while (myfile >> word)
+    {
+        if (word.find(motive) != string::npos)
+            count++;
+    }
+    myfile.close();
+    
+    cout << "The file " + path + " contains " << count 
+         << " words containing the motive " << motive << endl;
 
     return 0;
 }
